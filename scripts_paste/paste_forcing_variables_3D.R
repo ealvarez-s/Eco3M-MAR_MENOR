dc6d503a6c4aa912f5f7b4b91cb6010d585a5f75
ealvarez-s <ealvarez@inogs.it>
2024-10-04 14:55:15 +0200

    updated pasting scripts

---
+++ scripts_paste/paste_forcing_variables_3D.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
+
+rdir <-as.character(args[1])
+var <- as.character(args[2])
+sdir <-as.character(args[3])
+
+if (require(RNetCDF)==FALSE) install.packages("RNetCDF")
+#if (require(abind)==FALSE) install.packages("abind")
+library(RNetCDF)
+#library(abind)
+
+  archivo<-setdiff(list.files(rdir,pattern=".nc"),list.files(rdir,pattern="ECO3M-S"))
+  eliminar<-list.files(rdir,pattern=".bkp")
+  archivo<-setdiff(archivo, eliminar)
+  fecha<-substring(archivo,1,8)
+  hora<-substring(archivo,10,15)
+  nombre<-unique(substring(archivo,1,15))
+  tiempo<-rep(NA,length(archivo))
+  arrayEd<-array(data=NA, dim=c(432,532,20,length(archivo)),dimnames=NULL)
+
+  for (k in 1:length(archivo)){
+      archivoEd <-archivo[k]
+      filename <- paste(rdir,archivoEd,sep="/")
+      filenc <- open.nc(filename)
+      #print.nc(filenc)
+      filerc <- read.nc(filenc)
+      tiempo[k]<-filerc$time
+      Ed<-filerc[[which(names(filerc)==var)]]
+      arrayEd[,,,k]<-Ed
+      print(archivoEd)
+  } # end loop k
+  save(tiempo,arrayEd,file=paste(sdir,"/",var,"_3D.RData",sep=""))
---
+++ scripts_paste/sequence_olympe.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+#conda activate to_run_R
+
+INDIR3=/tmpdir/alvarez/MAR_MENOR/BGC_MAR_MENOR/GRAPHIQUES/RUN_reference_10days_output
+OUTDIR=/tmpdir/alvarez/MAR_MENOR/BGC_MAR_MENOR/GRAPHIQUES/RUN_reference_10days_output
+CODEDIR=/tmpdir/alvarez/MAR_MENOR/scripts_paste
+
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} z_eu ${OUTDIR}
+
+Rscript ${CODEDIR}/paste_supp_variables_3D.R ${INDIR3} PAR ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_3D.R ${INDIR3} pH ${OUTDIR}
+
+Rscript ${CODEDIR}/paste_forcing_variables_3D.R ${INDIR3} tem ${OUTDIR}
+Rscript ${CODEDIR}/paste_forcing_variables_3D.R ${INDIR3} sal ${OUTDIR}
+
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} zoonanoc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} zoomicroc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} zoomesoc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} synec ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} synen ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} synep ${OUTDIR}
+Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} synechl ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} nanoc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} nanon ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} nanop ${OUTDIR}   
+Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} nanochl ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} diac ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} dian ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} diap ${OUTDIR}
+Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} diachl ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} diasi ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} bactc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} smopc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} smopn ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} smopp ${OUTDIR}
+Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} smopchl ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} smopsi ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} lmopc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} lmopn ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} lmopp ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} lmopsi ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} modc ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} modn ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} modp ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} nitrate ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} ammonium ${OUTDIR}  
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} phosphate ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} silice ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} oxygen ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} odu ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} dic ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} talk ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} density ${OUTDIR}
