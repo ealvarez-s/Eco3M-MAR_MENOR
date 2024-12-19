d35304a942767b614dfe23f3d606041385473728
ealvarez-s <ealvarez@inogs.it>
2024-12-19 17:21:43 +0100

    updated postproc with 2D supp fluxes

--- scripts_postproc/sequence_olympe.sh
+++ scripts_postproc/sequence_olympe.sh
@@ -2,17 +2,27 @@
 
 #conda activate to_run_R
 
-INDIR3=/tmpdir/alvarez/MAR_MENOR/BGC_MAR_MENOR/GRAPHIQUES/RUN_reference_10days_output
-OUTDIR=/tmpdir/alvarez/MAR_MENOR/BGC_MAR_MENOR/GRAPHIQUES/RUN_reference_10days_output
+INDIR3=/tmpdir/alvarez/MAR_MENOR/BGC_MAR_MENOR/GRAPHIQUES/RUN_reference_10days_reflect
+OUTDIR=/tmpdir/alvarez/MAR_MENOR/BGC_MAR_MENOR/GRAPHIQUES/RUN_reference_10days_reflect
 CODEDIR=/tmpdir/alvarez/MAR_MENOR/scripts_postproc
 
-Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} z_eu ${OUTDIR}
+#Rscript ${CODEDIR}/paste_state_variables_3D_xyear.R ${INDIR3} oxygen 2017 ${OUTDIR}
+
+##Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} z_eu ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} Pefflux2d ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} Siefflux2d ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} NO3efflux2d ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} NH4efflux2d ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} DICefflux2d ${OUTDIR}
+Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} 02influx2d ${OUTDIR}
+
+#Rscript ${CODEDIR}/paste_supp_variables_2D.R ${INDIR3} CDepo ${OUTDIR}
 
 Rscript ${CODEDIR}/paste_supp_variables_3D.R ${INDIR3} PAR ${OUTDIR}
 Rscript ${CODEDIR}/paste_supp_variables_3D.R ${INDIR3} pH ${OUTDIR}
 
-Rscript ${CODEDIR}/paste_forcing_variables_3D.R ${INDIR3} tem ${OUTDIR}
-Rscript ${CODEDIR}/paste_forcing_variables_3D.R ${INDIR3} sal ${OUTDIR}
+#Rscript ${CODEDIR}/paste_forcing_variables_3D.R ${INDIR3} tem ${OUTDIR}
+#Rscript ${CODEDIR}/paste_forcing_variables_3D.R ${INDIR3} sal ${OUTDIR}
 
 #Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} zoonanoc ${OUTDIR}
 #Rscript ${CODEDIR}/paste_state_variables_3D.R ${INDIR3} zoomicroc ${OUTDIR}
